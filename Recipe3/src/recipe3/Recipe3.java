1f843afb1096d92c874fc083dd5fedca8c7a50d5
11hurdj <hurdjg@mail.masters.edu>
2013-02-21 20:48:55 -0800

    Cleanup

--- Recipe3/src/recipe3/Recipe3.java
+++ Recipe3/src/recipe3/Recipe3.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package recipe3;
 
 /**
@@ -11,10 +7,6 @@ package recipe3;
  * @author John
  */
 public class Recipe3 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
