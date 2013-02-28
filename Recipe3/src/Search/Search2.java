83d5c4ce8e3e92402d6ab244b5ea9ee0ac2ac304
Jake <webbjake32@yahoo.com>
2013-02-27 18:36:03 -0800

    search update

---
+++ Recipe3/src/Search/Search2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Search;
+
+/**
+ *
+ * @author jakewebb
+ */
+public class Search2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
