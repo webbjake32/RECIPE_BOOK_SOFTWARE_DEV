c87e7f172055418f1caeade233a090203cf36e5f
11hurdj <hurdjg@mail.masters.edu>
2013-02-27 17:21:24 -0800

    Update..

--- Recipe3/src/recipe3/Recipe.java
+++ Recipe3/src/recipe3/Recipe.java
@@ -17,7 +17,37 @@ public class Recipe {
     private String[] comments;
     private int rating;
     
-    public void add(){
-        
+    /**
+     * This method takes the user's input (all input variables signified by the
+     * name "in___") and assigns it to the class variables above.
+     */
+    public void add(String inTitle, String[] inMainIngredients, double[] inQuantities,
+            String[] inAltIngredients, double[] inAltQuantities, 
+            String[] inInstructions, int inPrepTime, int inCategory, int inSubCategory, 
+            String[] inComments, int inRating){
+        title = inTitle;
+        mainIngredients = inMainIngredients;
+        quantities = inQuantities;
+        altIngredients = inAltIngredients;
+        altQuantities = inAltQuantities;
+        instructions = inInstructions;
+        prepTime = inPrepTime;
+        category = inCategory;
+        subCategory = inSubCategory;
+        comments = inComments;
+        rating = inRating;
+    }
+    
+    @Override
+    public String toString(){
+        String output = "" + title + "";    //ADD MORE HERE LATER
+        return output;
     }
 }
+
+/**
+ * add(String title, String[] mainIngredients, double[] quantities,
+            String[] altIngredients, double[] altQuantities, 
+            String[] instructions, int prepTime, int category, int subCategory, 
+            String[] comments, int rating){
+ */
