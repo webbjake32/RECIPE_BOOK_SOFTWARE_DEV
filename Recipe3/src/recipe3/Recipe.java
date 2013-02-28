1a97a43bd756a4bdde93152f56cc114ef8e6558f
11hurdj <hurdjg@mail.masters.edu>
2013-02-27 16:01:52 -0800

    Update..

--- Recipe3/src/recipe3/Recipe.java
+++ Recipe3/src/recipe3/Recipe.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package recipe3;
 
 /**
@@ -9,5 +5,5 @@ package recipe3;
  * @author John
  */
 public class Recipe {
-    
+    private String title;
 }
